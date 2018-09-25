8dc29a3ee83a6274e5cfe2f7001bbcf331077103
YuTing Huang <yutingeko@gmail.com>
2018-09-25 22:56:04 +0800

    slideshow layout

--- src/js/components/presentational/spot_detail_content.js
+++ src/js/components/presentational/spot_detail_content.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import SlideShow from '../widget/slideshow';
 
 const SpotDetailContent = ({detail, images}) => {
   return (
-    <div>
-      <h3>{detail.Name}</h3>
-      <div>{images.map(({thumbnail, url}, i) => (
-        <img key={i} src={thumbnail} alt={detail.name}/>
-      ))}
+    <div className="media">
+      <div className="media-left">
+        <SlideShow
+          images={images}
+          title={detail.Name}
+          alt={`${detail.Name}-相關照片`}
+          amount={[1, 5]}
+          width="500"
+        />
+      </div>
+      <div className="media-body">
+      <h1>{detail.Name}</h1>
       </div>
     </div>
   );
---
+++ src/js/components/widget/slideshow.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import '../../../css/slideshow.css'
+
+class SlideShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImg: 0
+    };
+
+    // images={images}
+    // title={detail.Name}
+    // alt={`${detail.Name}-相關照片`}
+    // amount={`[500, 100]`}
+
+
+  }
+
+  showImages(amount, key) {
+    let width = this.props.width;
+    let gap = 10;
+    let totalGap = (amount + 1) * 10;
+    let eachImgLength = Math.floor((width - totalGap) / amount);
+    let eachImgLengthWithMargin = eachImgLength + gap;
+    let images = this.props.images;
+    let imagesAmount = images.length;
+    let matchImages = images.map(({thumbnail, image}) => {
+      return amount === 1 ? image : thumbnail;
+    });
+
+    return (
+      <div
+        key={key}
+        style={ { width: `${width}px`, height: `${eachImgLengthWithMargin}px` } }
+        className="slideshow-container"
+      >
+        <a
+          className="slideshow-arrow"
+          style={{ height: `${eachImgLength}px` }}
+        >&lt;</a>
+        <a
+          className="slideshow-arrow"
+          style={{ height: `${eachImgLength}px`, right: 0 }}
+        >&gt;</a>
+        <div
+          className="slideshow-image-container"
+          style={ { width: `${eachImgLength }px`, height: `${eachImgLength}px` } }
+        >
+          <ul
+            className="slideshow-image-wrap"
+            style={ { width: `${eachImgLength * imagesAmount }px`, height: `${eachImgLength}px` } }
+          >
+            {matchImages.map((image, i) => (
+              <li
+                key={i}
+                style={ {
+                  width: `${eachImgLength}px`,
+                  height: `${eachImgLength}px`,
+                  margin: `${gap}px`
+                } }
+                className="align-center">
+                <img
+                  src={image}
+                  alt={this.props.alt}
+                  title={this.props.title}
+                  className="slideshow-image-img"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    let hasImages = this.props.images.length > 0;
+    return (
+      <div>
+        {
+          hasImages && this.props.amount.map((amount, i) => this.showImages(amount, i) )
+        }
+
+
+        {/* { currentImgUrl && <img src={ currentImgUrl } alt={this.props.alt}/> }
+        <ul>
+          {this.props.images.map(({thumbnail, url}, i) => (
+            <li key={i}><img src={thumbnail} alt={this.props.alt}/></li>
+          ))}
+        </ul> */}
+
+
+      </div>
+    );
+  }
+}
+
+export default SlideShow;

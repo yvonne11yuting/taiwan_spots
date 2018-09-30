bf6bfb8cb57f0d1ce422ad72d462a5a6bf83902b
YuTing Huang <yutingeko@gmail.com>
2018-09-30 14:13:01 +0800

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const devMode = process.env.NODE_ENV !== 'production';
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   optimization: {
@@ -13,26 +14,41 @@ module.exports = {
         sourceMap: true
       }),
       new OptimizeCSSAssetsPlugin({})
-    ],
-    // splitChunks: {
-    //   cacheGroups: {
-    //     styles: {
-    //       name: 'styles',
-    //       test: /\.css$/,
-    //       chunks: 'all',
-    //       enforce: true
-    //     }
-    //   }
-    // }
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
+      favicon: './src/favicon.ico',
     }),
     new MiniCssExtractPlugin({
       filename: devMode ? 'styles/[name].css' : 'styles/[name].[hash].css',
       chunkFilename: devMode ? 'styles/[id].css' : 'styles/[id].[hash].css',
+    }),
+    new ManifestPlugin({
+      seed: {
+        "name": "休假去哪裡",
+        "short_name": "休假去哪裡",
+        "start_url": ".",
+        "theme_color": "#2cc3ff",
+        "display": "standalone",
+        "background_color": "#2cc3ff",
+        "description": "休假去哪裡，台灣好景點",
+        "icons": [
+          {
+            "src": "/android-icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          }
+        ],
+        "related_applications": [
+          {
+            "platform": "web"
+          }
+        ],
+        "orientation": "portrait"
+      }
     })
   ],
   module: {
